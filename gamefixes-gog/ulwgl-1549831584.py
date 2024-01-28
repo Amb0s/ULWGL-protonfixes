a8bdcf2b9b981bfd360be0554e97601b504b901f
Amb0s <ambos@disroot.org>
2024-01-28 16:48:16 +0100

    Add Beat Angel Escalayer R game fix

---
+++ gamefixes-gog/ulwgl-1549831584.py
@@ -0,0 +1,22 @@
+""" Game fix for Beat Angel Escalayer R
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ installs d3dx9, dirac, dotnet35, dotnet40, lavfilters, vcrun2005, vcrun2008
+    """
+
+    util.protontricks('d3dx9')
+    util.protontricks('dirac')
+    util.protontricks('dotnet35')
+    util.protontricks('dotnet40')
+    util.protontricks('lavfilters')
+    util.protontricks('vcrun2005')
+    util.protontricks('vcrun2008')
+    util.disable_nvapi()
+    util.disable_dxvk()
+    util.disable_esync()
+    util.disable_fsync()
+    util.disable_protonaudioconverter()
