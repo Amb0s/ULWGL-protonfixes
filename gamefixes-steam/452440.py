08423214e65e811b7b215a5463a01ac23cc4f207
Amb0s <ambos@disroot.org>
2024-01-28 16:41:58 +0100

    Add Flowers -Le volume sur printemps- game fix

---
+++ gamefixes-steam/452440.py
@@ -0,0 +1,20 @@
+""" Game fix for Flowers -Le volume sur printemps- 
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ installs d3dx9, dirac, lavfilters, vcrun2005, vcrun2008
+    """
+
+    util.protontricks('d3dx9')
+    util.protontricks('dirac')
+    util.protontricks('lavfilters')
+    util.protontricks('vcrun2005')
+    util.protontricks('vcrun2008')
+    util.disable_nvapi()
+    util.disable_dxvk()
+    util.disable_esync()
+    util.disable_fsync()
+    util.disable_protonaudioconverter()
