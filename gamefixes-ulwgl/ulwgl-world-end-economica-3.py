e7316cafa94efc33d5d86c532db2d39542304d08
Amb0s <ambos@disroot.org>
2024-01-29 21:04:29 +0100

    Add WORLD END ECONOMiCA episode.03 game fix

---
+++ gamefixes-ulwgl/ulwgl-world-end-economica-3.py
@@ -0,0 +1,30 @@
+""" Game fix for WORLD END ECONOMiCA episode.03
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ Installs d3dx9, dirac, dotnet35, dotnet40, lavfilters, vcrun2005, vcrun2008
+    """
+
+    util.protontricks('d3dx9')
+    util.protontricks('dirac')
+    util.protontricks('lavfilters')
+    util.protontricks('vcrun2005')
+    util.protontricks('vcrun2008')
+
+    """ Prevents the window manager from decorating and controlling windows
+    """
+
+    util.protontricks('windowmanagerdecorated=n')
+    util.protontricks('windowmanagermanaged=n')
+
+    """ Disables NvAPI, DXVK, Esync, Fsync and protonaudioconverter
+    """
+
+    util.disable_nvapi()
+    util.disable_dxvk()
+    util.disable_esync()
+    util.disable_fsync()
+    util.disable_protonaudioconverter()
