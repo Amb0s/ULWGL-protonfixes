9eabe80941f95b2483875160a08184884a150e5a
Amb0s <ambos@disroot.org>
2024-01-31 22:47:38 +0100

    Add Cartagra ~Affliction of the Soul~ game fix

---
+++ gamefixes-ulwgl/ulwgl-cartagra.py
@@ -0,0 +1,24 @@
+""" Game fix for Cartagra ~Affliction of the Soul~
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ Installs d3dx9, dirac, lavfilters, vcrun2005, vcrun2008
+    """
+
+    util.protontricks('d3dx9')
+    util.protontricks('dirac')
+    util.protontricks('lavfilters')
+    util.protontricks('vcrun2005')
+    util.protontricks('vcrun2008')
+
+    """ Disables NvAPI, DXVK, Esync, Fsync and protonaudioconverter
+    """
+
+    util.disable_nvapi()
+    util.disable_dxvk()
+    util.disable_esync()
+    util.disable_fsync()
+    util.disable_protonaudioconverter()
