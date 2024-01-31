0f151c34f59c81732c33868374b8667c5ca369a1
Amb0s <ambos@disroot.org>
2024-01-31 16:13:14 +0100

    Add Mahoutsukai no Yoru game fix

---
+++ gamefixes-ulwgl/ulwgl-mahoyo.py
@@ -0,0 +1,38 @@
+""" Game fix for Mahoutsukai no Yoru
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ Installs cjkfonts, d3dx9, dirac, dotnet35, dotnet40, lavfilters, vcrun2005, vcrun2008
+    """
+
+    util.protontricks('cjkfonts')
+    util.protontricks('d3dx9')
+    util.protontricks('dirac')
+    util.protontricks('dotnet35')
+    util.protontricks('dotnet40')
+    util.protontricks('lavfilters')
+    util.protontricks('vcrun2005')
+    util.protontricks('vcrun2008')
+
+    """ Changes renderer to GDI
+    """
+
+    util.protontricks('renderer=gdi')
+
+    """ Sets Japanese locale and change time zone
+    """
+
+    util.set_environment('LC_ALL', 'ja_JP.UTF-8')
+    util.set_environment('TZ', 'Asia/Tokyo')
+
+    """ Disables NvAPI, DXVK, Esync, Fsync and protonaudioconverter
+    """
+
+    util.disable_nvapi()
+    util.disable_dxvk()
+    util.disable_esync()
+    util.disable_fsync()
+    util.disable_protonaudioconverter()
