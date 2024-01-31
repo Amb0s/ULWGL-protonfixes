4a5c2a73064701775f6b18e283d7c89ddece5735
Amb0s <ambos@disroot.org>
2024-01-31 16:10:54 +0100

    Add Hanachirasu game fix

---
+++ gamefixes-ulwgl/ulwgl-hanachirasu.py
@@ -0,0 +1,31 @@
+""" Game fix for Hanachirasu
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ Installs d3dx9, dirac, dotnet35, dotnet40, lavfilters, vcrun2005, vcrun2008
+    """
+
+    util.protontricks('d3dx9')
+    util.protontricks('dirac')
+    util.protontricks('lavfilters')
+    util.protontricks('vcrun2005')
+    util.protontricks('vcrun2008')
+
+    """ Sets virtual desktop
+    """
+
+    screen_width,screen_height = util.get_resolution()
+    screen_resolution = str(screen_width) + 'x' + str(screen_height)
+    util.protontricks('vd=' + screen_resolution)
+
+    """ Disables NvAPI, DXVK, Esync, Fsync and protonaudioconverter
+    """
+
+    util.disable_nvapi()
+    util.disable_dxvk()
+    util.disable_esync()
+    util.disable_fsync()
+    util.disable_protonaudioconverter()
