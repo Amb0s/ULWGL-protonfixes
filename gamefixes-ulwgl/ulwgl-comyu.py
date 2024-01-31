8e6eb072e6e95ad3a923c9d89f93f5a32be6637e
Amb0s <ambos@disroot.org>
2024-01-31 16:01:48 +0100

    Add Coμ - Kuroi Ryuu to Yasashii Oukoku - game fix

---
+++ gamefixes-ulwgl/ulwgl-comyu.py
@@ -0,0 +1,47 @@
+""" Game fix for Coμ - Kuroi Ryuu to Yasashii Oukoku -
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    # Should use a 32-bit prefix?
+
+    """ Installs cjkfonts, d3dx9, dirac, dotnet35, dotnet40, lavfilters, vcrun2005, vcrun2008
+    """
+
+    util.protontricks('cjkfonts')
+    util.protontricks('d3dx9')
+    util.protontricks('dirac')
+    util.protontricks('dotnet35')
+    util.protontricks('dotnet40')
+    util.protontricks('lavfilters')
+    util.protontricks('vcrun2005')
+    util.protontricks('vcrun2008')
+
+    """ Changes renderer to GDI
+    """
+
+    util.protontricks('renderer=gdi')
+
+    """ Sets virtual desktop
+    """
+
+    screen_width,screen_height = util.get_resolution()
+    screen_resolution = str(screen_width) + 'x' + str(screen_height)  
+    util.protontricks('vd=' + screen_resolution)
+
+    """ Sets Japanese locale and change time zone
+    """
+
+    util.set_environment('LC_ALL', 'ja_JP.UTF-8')
+    util.set_environment('TZ', 'Asia/Tokyo')
+
+    """ Disables NvAPI, DXVK, Esync, Fsync and protonaudioconverter
+    """
+
+    util.disable_nvapi()
+    util.disable_dxvk()
+    util.disable_esync()
+    util.disable_fsync()
+    util.disable_protonaudioconverter()
