c7a5fc4ee1b95d97075e4bac5c7a3671a38f7c06
Amb0s <ambos@disroot.org>
2024-02-18 12:48:58 +0100

    Add Saya no Uta ~ The Song of Saya game fix

---
+++ gamefixes-ulwgl/ulwgl-saya-no-uta.py
@@ -0,0 +1,26 @@
+""" Game fix for Saya no Uta ~ The Song of Saya
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ Installs d3dx9, dirac, dotnet35, dotnet40, lavfilters, vcrun2005, vcrun2008
+    """
+
+    util.protontricks('d3dx9')
+    util.protontricks('dirac')
+    util.protontricks('dotnet35')
+    util.protontricks('dotnet40')
+    util.protontricks('lavfilters')
+    util.protontricks('vcrun2005')
+    util.protontricks('vcrun2008')
+
+    """ Disables NvAPI, DXVK, Esync, Fsync and protonaudioconverter
+    """
+
+    util.disable_nvapi()
+    util.disable_dxvk()
+    util.disable_esync()
+    util.disable_fsync()
+    util.disable_protonaudioconverter()
