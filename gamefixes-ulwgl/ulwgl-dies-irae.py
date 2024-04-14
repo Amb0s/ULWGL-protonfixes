7a957df8fe304a24762a02dcdb776545c398efe6
Amb0s <ambos@disroot.org>
2024-04-14 10:00:38 +0200

    Add Dies irae game fix

---
+++ gamefixes-ulwgl/ulwgl-dies-irae.py
@@ -0,0 +1,39 @@
+""" Game fix for Dies irae ~Acta est Fabula~
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ Installs d3dx9, dirac, lavfilters, vcrun2005, vcrun2008
+    """
+
+    util.protontricks('d3dx9')
+    util.protontricks('dirac')
+    util.protontricks('lavfilters')
+    util.protontricks('vcrun2005')
+    util.protontricks('vcrun2008')
+    
+    """ Prevents the window manager from decorating and controlling windows
+    """
+
+    util.protontricks('windowmanagerdecorated=n')
+    util.protontricks('windowmanagermanaged=n')
+    
+    """ Sets virtual desktop
+    """
+
+    screen_width,screen_height = util.get_resolution()
+    screen_resolution = str(screen_width) + 'x' + str(screen_height)  
+    util.protontricks('vd=' + screen_resolution)
+
+    """ Disables NvAPI, DXVK, Esync, Fsync and protonaudioconverter
+    """
+
+    util.disable_nvapi()
+    util.disable_dxvk()
+    util.disable_esync()
+    util.disable_fsync()
+    util.disable_protonaudioconverter()
+
+
