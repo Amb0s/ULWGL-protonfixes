72bcce353e2f3ed136c1931a7f3322ff9e5f8f55
Amb0s <ambos@disroot.org>
2024-04-14 09:07:38 +0200

    Add eden* game fix

---
+++ gamefixes-ulwgl/ulwgl-eden.py
@@ -0,0 +1,32 @@
+""" Game fix for eden* PLUS＋MOSAIC
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ Installs d3dx9, dirac, lavfilters, vcrun2005, vcrun2008
+    """
+
+    util.protontricks('d3dx9')
+    util.protontricks('dirac')
+    util.protontricks('lavfilters')
+    util.protontricks('vcrun2005')
+    util.protontricks('vcrun2008')
+    
+    """ Prevents the window manager from decorating and controlling windows
+    """
+
+    util.protontricks('windowmanagerdecorated=n')
+    util.protontricks('windowmanagermanaged=n')
+
+    """ Disables NvAPI, DXVK, Esync, Fsync and protonaudioconverter
+    """
+
+    util.disable_nvapi()
+    util.disable_dxvk()
+    util.disable_esync()
+    util.disable_fsync()
+    util.disable_protonaudioconverter()
+
+
